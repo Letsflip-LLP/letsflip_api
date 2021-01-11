91a62e97e2e578692444520d9f39d3998b8517c5
saputroandhi <sepatuqu.robel@gmail..com>
2021-01-11 19:51:56 +0700

    handle diff date

--- app/Http/Helpers/FormatHelper.php
+++ app/Http/Helpers/FormatHelper.php
@@ -2,12 +2,41 @@
   
 function dateFormat($date){
     $sort = $date->diffForHumans(null,true);
-    $sort = explode(' ',$sort);
+    $sort = explode(' ',$sort); 
+
     return (object) [
         "date" =>\Carbon\Carbon::parse($date)->format("D, M Y"),
-        "diff" => $date->diffForHumans(),
+        "diff" => diffFormatTableOfTime($date),
         "sort_diff" => $sort[0]." ".strtoupper($sort[1][0]),
     ];
+}
+
+function diffFormatTableOfTime($date){
+    $diffSecond = \Carbon\Carbon::now()->diffInSeconds($date);
+ 
+    switch($diffSecond){
+        case $diffSecond <= 60 :
+            return "Just Now";
+        break;
+        case $diffSecond <= 900 :
+            return "Less than 15 mins ago";
+        break;
+        case $diffSecond <= 1800 :
+            return "Less than 30 mins ago";
+        break;
+        case $diffSecond <= 2700 :
+            return "Less than 45 mins ago";
+        break;
+        case $diffSecond <= 3600 :
+            return "Less than 1hr ago";
+        break;
+        case $diffSecond <= 43200 :
+            return "11 hrs";
+        break;
+        default:
+            return \Carbon\Carbon::parse($date)->format("M d").' at '.\Carbon\Carbon::parse($date)->format("g:i A");
+        break;
+    }
 }
 
 function defaultImage($module){
