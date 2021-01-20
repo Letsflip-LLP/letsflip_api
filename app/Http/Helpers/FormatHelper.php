6303f072d6f0153d61ca4894591665fbf4eec701
saputroandhi <sepatuqu.robel@gmail..com>
2021-01-20 21:48:25 +0700

    handle H to h

--- app/Http/Helpers/FormatHelper.php
+++ app/Http/Helpers/FormatHelper.php
@@ -7,7 +7,7 @@ function dateFormat($date){
     return (object) [
         "date" =>\Carbon\Carbon::parse($date)->format("D, M Y"),
         "diff" => diffFormatTableOfTime($date),
-        "sort_diff" => $sort[0]." ".strtoupper($sort[1][0]),
+        "sort_diff" => $sort[0]." ".strtolower($sort[1][0]),
     ];
 }
 
