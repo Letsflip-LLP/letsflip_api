9c9b8a11b7206679937db95fc68f879f26f960c2
saputroandhi <sepatuqu.robel@gmail..com>
2021-01-27 23:05:53 +0700

    handle like notif

--- app/Http/Controllers/V1/MissionController.php
+++ app/Http/Controllers/V1/MissionController.php
@@ -335,8 +335,20 @@ class MissionController extends Controller
        $model1 = $model1->where('user_id',$this->user_login->id);
 
        if($request->filled("mission_id")){
-             $model1 = $model1->where('mission_id',$request->mission_id);
+             $model1 = $model1->where('mission_id',$request->mission_id); 
              $model2->mission_id = $request->mission_id;
+              
+             // Notify to user  
+             $mission_detail = MissionModel::where('id',$request->mission_id)->first();
+             NotificationModel::insert([
+                 "id" =>  Uuid::uuid4(),
+                 "user_id_from"   => $this->user_login->id,
+                 "user_id_to"     => $mission_detail->user_id,
+                 "mission_id"     => $mission_detail->id,
+                 "created_at"     => date('Y-m-d H:i:s'),
+                 "updated_at"     => date('Y-m-d H:i:s'),
+                 "type"           => 3
+             ]);  
        }
 
         if($request->filled("mission_comment_id")){
--- resources/lang/en/notification.php
+++ resources/lang/en/notification.php
@@ -4,7 +4,8 @@
 return [
     "TEXT" => [
         1 => ":user_name_from has responds to your Mission : :module_title",
-        2 => ":user_name_from Created a mission in your Public Classroom : :module_title'"
+        2 => ":user_name_from Created a mission in your Public Classroom : :module_title'",
+        3 => ":user_name_from Liked your Mission : :module_title'"
     ],
     "TYPE" => [
         1 => "New respone from our mission",
