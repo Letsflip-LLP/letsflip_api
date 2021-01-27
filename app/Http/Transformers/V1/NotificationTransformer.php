5783d3719e5b97735be46d97d2c9748a3d6ae690
saputroandhi <sepatuqu.robel@gmail..com>
2021-01-27 23:14:18 +0700

    handle like mission

--- app/Http/Transformers/V1/NotificationTransformer.php
+++ app/Http/Transformers/V1/NotificationTransformer.php
@@ -13,7 +13,13 @@ class NotificationTransformer {
     public function item($model){ 
         $temp           = new \stdClass();
         $temp->id       = $model->id;
-        $temp->title    = __('notification.TEXT.'.$model->type,[ 'user_name_from' => $model->UserFrom->first_name.' '.$model->UserFrom->first_name , 'module_title' => $model->Mission->title]);         
+
+        if($model->type == 1 || $model->type== 2)
+            $temp->title    = __('notification.TEXT.'.$model->type,[ 'user_name_from' => $model->UserFrom->first_name.' '.$model->UserFrom->first_name , 'module_title' => $model->Mission->title]);    
+            
+        if($model->type ==  3)
+            $temp->title    = __('notification.TEXT.'.$model->type,[ 'user_name_from' => $model->UserFrom->first_name.' '.$model->UserFrom->first_name , 'module_title' => $model->Respone->title]);         
+        
         $temp->text     =   $temp->title;
         $temp->user     = UserTransformer::item($model->UserFrom);
 
--- resources/lang/en/notification.php
+++ resources/lang/en/notification.php
@@ -5,7 +5,8 @@ return [
     "TEXT" => [
         1 => ":user_name_from has responds to your Mission : :module_title",
         2 => ":user_name_from Created a mission in your Public Classroom : :module_title'",
-        3 => ":user_name_from Liked your Mission : :module_title'"
+        3 => ":user_name_from Liked your Mission : :module_title'",
+        4 => ":user_name_from Liked your Respone : :module_title'", 
     ],
     "TYPE" => [
         1 => "New respone from our mission",
