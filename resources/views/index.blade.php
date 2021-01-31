7e5d2935c92e6fa3bc386e4f1439c9b8ae5920ed
saputroandhi <sepatuqu.robel@gmail..com>
2021-01-31 16:02:21 +0700

    handle redirect

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,14 @@
 <?php
-// use Jenssegers\Agent\Agent;
-// $agent = new Agent(); 
+use Jenssegers\Agent\Agent;
+$agent = new Agent(); 
 
-// $os = $agent->platform(); 
+$os = $agent->platform(); 
 
 // if($os == 'ios') header('Location: letsflip://staging.getletsflip.com');
 // if($os == 'AndroidOS') header('Location: https://staging.getletsflip.com');
 
-// header('Location: https://getletsflip.com');
-
-// exit;
+// header('Location: https://staging.getletsflip.com');
+ 
 ?>
 
 
@@ -17,4 +16,11 @@
 <br>
 <a href="letsflip://staging.getletsflip.com">letsflip://staging.getletsflip.com</a> 
 <br>
-<a href="letsflip://staging.getletsflip.com">https://staging.getletsflip.com</a> 
+<a href="https://staging.getletsflip.com">https://staging.getletsflip.com</a> 
+
+
+<script>
+@if($os == 'IOS' || $os == 'ios')
+    window.location.href = "letsflip://staging.getletsflip.com"; 
+@endif
+</script>
