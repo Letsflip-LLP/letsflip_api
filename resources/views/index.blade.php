fa00b436d05e2cb6b21ba010f27858efa4fc8d9f
saputroandhi <sepatuqu.robel@gmail..com>
2021-01-31 15:36:38 +0700

    test redirect

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,13 @@
 <?php
 use Jenssegers\Agent\Agent;
 $agent = new Agent(); 
-dd($agent->platform()); 
+
+$os = $agent->platform(); 
+
+if($os == 'ios') header('Location: letsflip://staging.getletsflip.com');
+if($os == 'AndroidOS') header('Location: https://staging.getletsflip.com');
+
+header('Location: https://getletsflip.com');
+
+exit;
 ?>
