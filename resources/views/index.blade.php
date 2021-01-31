b3f6ed6a4b93e6614f2089987a368d69f7bba189
saputroandhi <sepatuqu.robel@gmail..com>
2021-01-31 15:50:01 +0700

    handle link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,20 @@
 <?php
-use Jenssegers\Agent\Agent;
-$agent = new Agent(); 
+// use Jenssegers\Agent\Agent;
+// $agent = new Agent(); 
 
-$os = $agent->platform(); 
+// $os = $agent->platform(); 
 
-if($os == 'ios') header('Location: letsflip://staging.getletsflip.com');
-if($os == 'AndroidOS') header('Location: https://staging.getletsflip.com');
+// if($os == 'ios') header('Location: letsflip://staging.getletsflip.com');
+// if($os == 'AndroidOS') header('Location: https://staging.getletsflip.com');
 
-header('Location: https://getletsflip.com');
+// header('Location: https://getletsflip.com');
 
-exit;
+// exit;
 ?>
+
+
+<a href="https://getletsflip.com">https://getletsflip.com</a>
+<br>
+<a href="letsflip://staging.getletsflip.com">letsflip://staging.getletsflip.com</a> 
+<br>
+<a href="letsflip://staging.getletsflip.com">https://staging.getletsflip.com</a> 
