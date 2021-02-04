4dc54a709bfc1cdc585eaad76b3f334ec5ecaa8c
saputroandhi <sepatuqu.robel@gmail..com>
2021-02-04 22:52:12 +0700

    handle mission id in notification?

--- app/Http/Transformers/V1/NotificationTransformer.php
+++ app/Http/Transformers/V1/NotificationTransformer.php
@@ -7,14 +7,19 @@ use App\Http\Transformers\ResponseTransformer;
 use Carbon\Carbon; 
 use Illuminate\Support\Facades\Storage;
 use App\Http\Transformers\V1\UserTransformer; 
+use App\Http\Transformers\V1\MissionTransformer; 
 
 class NotificationTransformer {
  
-    public static function item($model){ 
+    public static function item($model){  
         $temp           = new \stdClass();
         $temp->id       = $model->id;
         $temp->text     = "";
         $temp->title    = "";
+
+        $temp->mission_detail = $model->Mission ? (object) [
+            "id" => $model->mission_id
+        ]:null;
 
         if($model->type == 1 || $model->type== 2)
             $temp->title    = __('notification.TEXT.'.$model->type,[ 'user_name_from' => $model->UserFrom->first_name.' '.$model->UserFrom->first_name , 'module_title' => $model->Mission->title]);    
