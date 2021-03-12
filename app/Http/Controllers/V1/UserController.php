3048ad309834883eb75f55da766bc5bc7c257206
saputroandhi <sepatuqu.robel@gmail..com>
2021-03-12 19:11:55 +0700

    validity on subscribe

--- app/Http/Controllers/V1/UserController.php
+++ app/Http/Controllers/V1/UserController.php
@@ -15,11 +15,20 @@ use App\Http\Models\UserFollowModel;
 use Ramsey\Uuid\Uuid;
 use App\Http\Libraries\StorageCdn\StorageManager;
 use Illuminate\Support\Facades\App;
-
+use Carbon\Carbon;
 use DB;
+use App\Http\Models\SubscriberModel; 
 
 class UserController extends Controller
 { 
+     //
+     private $user_login;
+
+     public function __construct(){
+         $this->user_login =  auth('api')->user();
+     }
+    
+     
     public function self(Request $request)
     { 
         $user = auth('api')->user(); 
@@ -165,5 +174,107 @@ class UserController extends Controller
     public function availlableSocialMedia(Request $request){
         $static_data = config('database.static_data.social_media_availlable');
         return (new ResponseTransformer)->toJson(200,__('messages.200'),$static_data);
+    }
+
+    private function _purchaseDetail($package,$sku,$token){
+
+        try{
+            $client = new \Google_Client();
+            $client->useApplicationDefaultCredentials();
+            $client->addScope('https://www.googleapis.com/auth/androidpublisher');
+            $service = new \Google_Service_AndroidPublisher($client);
+            $purchase = $service->purchases_subscriptions->get($package,$sku,$token);
+           
+            return $purchase;
+        } catch (\exception $exception){ 
+            return false;
+        }  
+    }
+
+    public function subscribePremiumAccount(Request $request){
+
+        DB::beginTransaction();
+        try {
+            
+            $product_account = config('account.premium_product');
+            $check_sub  = null;
+            $class_room = null;
+            $product_id = null;
+            $transaction_id = null;
+
+            if(!$request->filled('data'))
+                return (new ResponseTransformer)->toJson(400,__('messages.401'),false);
+
+            $vendor_payload = $request->data;  
+            $product_id = $vendor_payload['productId'];
+            $transaction_id = $vendor_payload['transactionId'];
+            $purchase_token = $vendor_payload['purchaseToken'];
+
+
+            $validate_token_purchase = $this->_purchaseDetail('com.lets_flip',$product_id,$purchase_token);
+
+            if($validate_token_purchase == false)
+                return (new ResponseTransformer)->toJson(400,__('messages.401'),false);
+
+            if($request->filled('classroom_id')){
+                $class_room = new ClassRoomModel;
+                $class_room = $class_room->where('id',$request->classroom_id)->first();
+                if($class_room == null)
+                    return (new ResponseTransformer)->toJson(400,__('messages.404'),false);
+            }
+
+            if(!isset($vendor_payload['productId']) || !isset($vendor_payload['transactionId']))
+                return (new ResponseTransformer)->toJson(400,__('messages.401'),false);
+
+    
+
+
+        if($this->user_login->Subscribe){
+            $check_sub = $this->user_login->Subscribe 
+            ->where('vendor_trx_id',$transaction_id)
+            ->first();
+        } 
+
+        if($check_sub)
+            return (new ResponseTransformer)->toJson(400,__('messages.401'),"Duplicated");
+
+        //START
+        $sub_end_date = isset($validate_token_purchase['expiryTimeMillis']) ? $validate_token_purchase['expiryTimeMillis'] : 0;
+        $sub_end_date = $sub_end_date / 1000;
+        $sub_end_date = date("Y-m-d H:i:s", $sub_end_date);
+
+        //END
+        $sub_start_date = isset($validate_token_purchase['startTimeMillis']) ? $validate_token_purchase['startTimeMillis'] : 0;
+        $sub_start_date = $sub_start_date / 1000;
+        $sub_start_date = date("Y-m-d H:i:s", $sub_start_date);
+        
+        $product_detail = $product_account[$product_id];
+        $subscribe =  SubscriberModel::firstOrCreate(
+            [ 
+                "user_id" => $this->user_login->id,
+                "status" => 1,
+                "vendor_trx_id" => $transaction_id
+            ],
+            [
+                "id"            => Uuid::uuid4(),
+                "date_start"    => $sub_start_date,//date('Y-m-d H:i:s'),
+                "date_end"      => $sub_end_date,//Carbon::now()->add('months',$product_detail['duration'])->format('Y-m-d H:i:s'),
+                "payload"       => json_encode($request->all()),
+                "type"          => $product_detail['type'],
+                "classroom_id"  => $class_room ? $class_room->id : null,
+                "product_id"    => $product_id
+            ]
+        );
+
+        DB::commit();
+    
+            return (new ResponseTransformer)->toJson(200,__('messages.200'), true);
+
+        } catch (\exception $exception){ 
+
+            DB::rollBack();
+
+            return (new ResponseTransformer)->toJson(500,$exception->getMessage(),false);
+        } 
     }
 }
