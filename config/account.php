b6847d2c4be5516de7282d33af42c2c986aa52ef
saputroandhi <sepatuqu.robel@gmail..com>
2021-04-05 09:25:25 +0700

    handle private account private_account

--- config/account.php
+++ config/account.php
@@ -15,6 +15,13 @@ return [
             "name" => env('STORE_SUB_PRIVATE_PRODUCT_NAME'),
             "price" => env('STORE_SUB_PRIVATE_PRODUCT_PRICE'),
             "duration" => env('STORE_SUB_PRIVATE_PRODUCT_VALIDITY')
+        ],
+        "private_account"=> [
+            "id" => env('STORE_SUB_PRIVATE_PRODUCT_ID'),
+            "type" => env('STORE_SUB_PRIVATE_PRODUCT_TYPE'),
+            "name" => env('STORE_SUB_PRIVATE_PRODUCT_NAME'),
+            "price" => env('STORE_SUB_PRIVATE_PRODUCT_PRICE'),
+            "duration" => env('STORE_SUB_PRIVATE_PRODUCT_VALIDITY')
         ]
     ], 
 ];
