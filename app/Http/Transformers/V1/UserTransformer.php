ecd69e91ddb1dd36fd98b06f4d72a0d7d91297f1
saputroandhi <sepatuqu.robel@gmail..com>
2021-05-09 21:30:13 +0700

    uuid

--- app/Http/Transformers/V1/UserTransformer.php
+++ app/Http/Transformers/V1/UserTransformer.php
@@ -35,6 +35,7 @@ class UserTransformer {
         if($sub && $sub->type > 1){
             $temp->type = subsType($sub->type);
             $temp->type->vendor_trx_id = $sub->vendor_trx_id;
+            $temp->type->uuid = $sub->id;
         }
 
         $temp->point      = $model->Point->sum('value');
